f200b82494e5a0d86e494f62b7c3eb313a996477
JAIME ANDRES POLANCO JIMENEZ <jaime.polanco@javeriana.edu.co>
2022-09-02 11:44:44 -0500

    previus to reinstall R

--- functions.R
+++ functions.R
@@ -1,19 +1,23 @@
 #################################
-EPS = "RES014"
+connection_string = "Provider=MSOLAP;Data Source=cubos.sispro.gov.co;Password=u4_gu41n14;
+            Persist Security Info=True;
+            User ID=sispro.local\\UA_Guainia;
+            Initial Catalog=SGD_ReportesRIPS;
+            Data Source=cubos.sispro.gov.co"
+
+VAR_INTERES = 'Condiciones transmisibles y nutricionales' ### Looped variable
+EPS = "RES014"   ### Looped variable
+
 SEGREGATION_EPS_CODE = '[Administradoras].[Codigo de Administradora]'
 EPS_CODE = '[Administradoras].[Codigo de Administradora]'
-SEGREGATION_VAR_INTERES = '[Causas de Morbilidad].[Gran Causa]'
+SEGREGATION_VAR_INTERES = '[Causas de Morbilidad].[Gran Causa]' 
 AXIS0 <- '[Measures].[ValorIndicador]'
 AXIS1 <- '[Tiempo].[Año - Semestre - Trimestre - Mes].[Año]'
 AXIS2 <- '[Municipio Residencia - RIPS].[Municipio]'
 from_olap_catalog = 'CU - Morbilidad_ASIS'
 TYPE_USER = '1 - CONTRIBUTIVO'
-VAR_INTERES = 'Condiciones transmisibles y nutricionales'
-query_cube_mdx(AXIS0 = AXIS0, 
-               AXIS1 = AXIS1,
-               AXIS2 = AXIS2,
-               cube = from_olap_catalog,
-               )
+
+
 
 mdx = query_cube_mdx(AXIS0 = AXIS0, 
                AXIS1 = AXIS1,
@@ -23,18 +27,27 @@ mdx = query_cube_mdx(AXIS0 = AXIS0,
                VAR_INTERES=VAR_INTERES, 
                SEGREGATION_EPS_CODE=SEGREGATION_EPS_CODE  , 
                EPS=EPS,  
-               cube = from_olap_catalog
-)
+               cube = from_olap_catalog )
 
-execue_query_mdx <- function(mdx, )}{
-  olapCnn<-olapR::OlapConnection(cnnstr)
+ 
+
+execue_query_mdx(mdx =mdx ,
+                 connection_string = connection_string, 
+                 EPS=EPS,
+                 VAR_INTERES=VAR_INTERES, 
+                 TYPE_USER= TYPE_USER  )
+
+
+execue_query_mdx <- function(mdx,connection_string, EPS,VAR_INTERES, TYPE_USER  ){
+  olapCnn<-olapR::OlapConnection(connection_string)
   tempo3 <- olapR::execute2D(olapCnn, mdx)
   tempo3[[2]] < - as.numeric(tempo3[[2]])
   tempo3 = subset(tempo3, tempo3[[2]] >= 2009 &  tempo3[[2]]  <=2022)
   colnames(tempo3) <- c('CITY', 'YEAR', 'VALUE')
   tempo3$EPS <- EPS
-  tempo3$GRAN_CAUSA <- as.character(GRAN_CAUSA)
+  tempo3$VAR_INTERES <- as.character(VAR_INTERES)
   tempo3$TYPE_USER <- as.character(TYPE_USER)
+  return(tempo3)
 }
 
 
