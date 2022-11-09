a45eea2c04c42551672876a0cea6bb82547131d7
JAIME ANDRES POLANCO JIMENEZ <jaime.polanco@javeriana.edu.co>
2022-11-08 20:41:06 -0500

    2022-11-08

--- Scripts/quality_index_eps_level.R
+++ Scripts/quality_index_eps_level.R
@@ -23,100 +23,178 @@ path_output = "C:/Users/USER/OneDrive - Pontificia Universidad Javeriana/02_UPJ 
 ##############                           
 library(readxl)
 if(1==1){
-asegurados_Tipo_regimen <- read_excel(paste0(path_output, "asegurados_caracteristicas.xlsx" ) , 
-                                         sheet = "Tipo_regimen")
-asegurados_com_indigena <- read_excel(paste0(path_output, "asegurados_caracteristicas.xlsx" ) , 
-                                         sheet = "com_indigena")
+    asegurados_Tipo_regimen <- read_excel(paste0(path_output, "asegurados_caracteristicas.xlsx" ) , 
+                                             sheet = "Tipo_regimen")
+    asegurados_com_indigena <- read_excel(paste0(path_output, "asegurados_caracteristicas.xlsx" ) , 
+                                             sheet = "com_indigena")
+    
+    asegurados_genero <- read_excel(paste0(path_output, "asegurados_caracteristicas.xlsx" ) , 
+                                             sheet = "genero")
+    asegurados_etarios <- read_excel(paste0(path_output, "asegurados_etarios.xlsx" ) )  
+                                     
+    asegurados <- read_excel(paste0(path_output, "asegurados.xlsx" ), 
+                             col_types = c("text", "numeric", "numeric", 
+                                           "numeric", "numeric", "numeric", 
+                                           "numeric", "numeric", "numeric", 
+                                           "numeric", "numeric", "numeric", 
+                                           "numeric", "numeric", "numeric", 
+                                           "skip", "skip", "skip", "skip"))
+    
+    library(readxl)
+    fecha_inicio <- read_excel(paste0(path_output, "fecha_inicio_actividades_administradoras.xlsx" ) ,
+                                                           sheet = "eapb", col_types = c("text", 
+                                                                                  "text", "numeric", "text", "numeric", 
+                                                                                         "numeric", "date", "text"))
 
-asegurados_genero <- read_excel(paste0(path_output, "asegurados_caracteristicas.xlsx" ) , 
-                                         sheet = "genero")
-asegurados_etarios <- read_excel(paste0(path_output, "asegurados_etarios.xlsx" ) )  
-                                 
-asegurados <- read_excel(paste0(path_output, "asegurados.xlsx" ), 
-                         col_types = c("text", "numeric", "numeric", 
-                                       "numeric", "numeric", "numeric", 
-                                       "numeric", "numeric", "numeric", 
-                                       "numeric", "numeric", "numeric", 
-                                       "numeric", "numeric", "numeric", 
-                                       "skip", "skip", "skip", "skip"))
+    fecha_inicio = data.frame('fec_inact' = c(
+      '2016' ,  '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2015' , '2015' , '2014' , '2016' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2016' , '2017' , '2014' , '2016' , '2016' , '2014' , '2014' , '2014' , '2014' , '2016' , '2014' , 
+      '2014' , '2014' , '2014' , '2016' , '2016' , '2016' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2016' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2017' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2016' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2016' , '2014' , '2014' , '2014' , '2014' , '2015' , '2015' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2016' , '2016' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2016' , '2014' , '2016' , 
+      '2014' , '2017' , '2014' , '2015' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , '2014' , 
+      '2016' , '2016' , '2016' , '2016' , '2016' , '2018' , '2017' , '2017' ) 
+      ,
+      
+      'eps' = c(
+        '37177' , '37024' , 'ESS208' , 'EMP024' , 'CCF017' , 'SAP034' , 'SAP031' , 'ESS177' , 'EPS103' , 'ESS061' , 'ESS107' , 'PEC005' , 'PEC002' , 'EPS027' , 
+        'PEC006' , '14-26' , '13-41' , 'CCF018' , 'EPS003' , 'EMP002' , 'CCF101' , 'UT-004' , 'EAS009' , 'EAS017' , 'EAS019' , 'REUE02' , 'EAS021' , 'EAS020' , 
+        'EAS003' , 'EAS008' , 'EAS018' , 'UT-001' , 'EPS028' , 'EPS020' , 'CCF004' , 'CCF016' , 'CCF039' , 'CCF030' , 'CCF020' , 'CCF012' , 'CCF014' , 'CCF013' , 
+        'CCF019' , 'CCF038' , 'CCF021' , 'CCF031' , 'CCF010' , 'CCF008' , 'CCF028' , 'CCF032' , 'CCF025' , 'CCF026' , 'CCF034' , 'CCF036' , '13-21' , 'EMP017' , 
+        'CCF002' , '37147' , '37238' , '13-42' , '37117' , '37209' , '13-18' , 'EPS030' , '14-14' , 'EAS013' , '05000' , 'REFM01' , 'REPN01' , 'EAS006' , 
+        'EAS014' , 'EAS005' , 'EPS029' , 'SAP030' , 'SAP008' , 'SAP026' , 'EAS029' , 'ESS115' , 'ESS164' , 'EPS129' , 'EPS130' , 'ESS122' , 'ESA016' , 'ESE113' , 
+        'ESS119' , 'ESS080' , 'ESS084' , 'ESS083' , 'ESS098' , 'ESS170' , 'ESS069' , 'ESS100' , 'ESS073' , 'ESS112' , 'ESS074' , 'ESS101' , 'ESS077' , 'ESS057' , 
+        'ESS175' , 'ESS060' , 'ESS114' , 'ESS178' , 'ESS017' , 'ESS071' , 'ESS066' ,'ESS019' , 'ESS051' , 'ESS047' , 'ESS179' , 'ESS187' , 'ESS056' , 'ESS012' , 
+        'ESS054' , 'ESS016' , 'ESS113' , 'ESS070' , 'ESS127' , 'ESS121' , 'ESS153' , 'ESS126' , 'ESS028' , 'ESS136' , 'ESS035' , 'ESS130' , 'ESS120' , 'ESS131' , 
+        'ESS128' , 'ESS095' , 'ESS193' , 'ESS140' , 'ESS143' , 'ESS144' , 'ESS205' , 'ESS191' , 'ESS196' , 'ESS151' , 'ESS186' , 'ESS195' , 'ESS200' , 'ESS105' , 
+        'ESS147' , 'ESS162' , 'ESS145' , 'ESS092' , 'ESS154' , 'ESS094' , 'ESS146' , 'ESS090' , 'ESS025' , 'ESS029' , 'ESS129' , 'ESS034' , 'ESS166' , 'ESS123' , 
+        'ESS110' , 'ESS015' , 'ESS109' , 'ESS021' , 'ESS132' , 'ESS046' , 'ESS163' , 'ESS093' , 'ESS088' , 'ESS096' , 'ESS032' , 'ESS085' , 'ESS192' , 'ESS201' , 
+        'ESS139' , 'ESS138' , 'ESS152' , 'ESS086' , 'ESS008' , 'ESS157' , 'ESS042' , 'ESS044' , 'ESS045' , 'ESS043' , 'ESS041' , 'ESS159' , 'ESS161' , 'ESS174' ,  
+        'ESS087' , 'ESS165' , 'ESS137' , 'ESS158' , 'ESS134' , 'PEC004' , 'REMG01' , 'EAS001' , 'EAS028' , 'EMP012' , 'EPS014' , 'RES006' , 'EAS022' , '36906' , 
+        '13-22' , '13-33' , 'EMP015' , 'EPS032' , '14-15' , '14-16' , 'REUE07' , 'EPS035' , 'EPS019' , '13-15' , 'SAP035' , '13-30' , '13-25' , '13-27' , 
+        '14-17' , 'SAP032' , 'REUE08' , 'EAS024' , 'EAS011' , 'EAS012' , 'SAP027' , 'ESS203' , 'REUE03' , 'REUE06' , 'REUE10' , 'EPS007' , 'UT-003' , 'REUE01' , 
+        'EMP022' , 'EPSS03' , 'EPSS26' , 'ESS182' , 'ESS188' , 'CCP053' , 'EPS133' , 'ARS003' , '14-1' , '14-4' , '14-5' , '14-7' , '14-8' , '25-10' , 'CCFC18' , 
+        'EPSMO3' , 'CCFC10' , 'EPSC20' , 'EPSS39' , 'EPS104' , 'EPSC03' , 'EPSM03' ) )
+    
+    fecha_inicio = eps_homog (Tabla =  fecha_inicio  , CODIGO__EPS = "eps" ) 
+    fecha_inicio$fec_inact = as.integer(fecha_inicio$fec_inact)
+    fecha_inicio = aggregate_function(aggregate = 'min',
+                                    cols_to_agg = colnames(fecha_inicio)[1]  ,
+                                    group_by = colnames(fecha_inicio)[3] ,
+                                    Tabla = fecha_inicio)
+     
+    fech_liqui = data.frame( "EPS" = c('EPS003', 'EPS013'	,  'EPS020' ,  'EPS024' , 'EPS035' ,  'EPS015' ,   'EPS014' ,   'EPS009' ,  'EPSC03' ,  'CCFC10' , 'CCFC18'),
+      	"fec_liqu" = c('2017' , '2015', '2015' , '2009' , '2011' , '2013' , '2013' ,  '2015' , '2017' , '2015' , '2015')
+    )
+    fech_liqui = eps_homog (Tabla =  fech_liqui  , CODIGO__EPS = "EPS" ) 
+    fech_liqui$fec_liqu = as.integer(fech_liqui$fec_liqu)
+    fech_liqui = aggregate_function(aggregate = 'min',
+                                      cols_to_agg = colnames(fech_liqui)[2]  ,
+                                      group_by = colnames(fech_liqui)[3] ,
+                                      Tabla = fech_liqui)
+    
+    fecha_inicio$fec_inact = as.character(fecha_inicio$fec_inact)
+    fech_liqui$fec_liqu = as.character(fech_liqui$fec_liqu)
+    library(tidyr)
+    
+    asegurados = asegurados %>% tidyr::pivot_longer(!eps, names_to = "Years", values_to = "Asegurados")         
+    asegurados_Tipo_regimen$year = as.character(asegurados_Tipo_regimen$year)
+    asegurados_genero$year = as.character(asegurados_genero$year)
+    asegurados_com_indigena$year = as.character(asegurados_com_indigena$year)
+    asegurados_etarios$year= as.character(asegurados_etarios$year)
+    
+     
+    asegurados = sqldf::sqldf("
+                 SELECT  A.*,B.TOTAL,  CONTRIBUTIVO , EXCEPCION , SUBSIDIADO   ,
+                 FEMENINO, MASCULINO , COMUNIDADES_INDIGENAS,
+                 De_0_a_9 , De_10_a_19 , De_20_a_29 , De_30_a_39, 
+                 De_40_a_49 , De_50_a_59 , De_60_a_69 ,
+                 De_70_a_79 , Mayor_De_80
+                 FROM asegurados A
+                 LEFT JOIN asegurados_Tipo_regimen B 
+                 ON TRIM(EPS)  = TRIM(B.COD_EPS)  AND TRIM(YEARS) = TRIM(B.YEAR)
+                 LEFT JOIN asegurados_genero C
+                 ON TRIM(EPS)  = TRIM(C.COD_ENTI)  AND TRIM(YEARS) = TRIM(C.YEAR)
+                 LEFT JOIN asegurados_com_indigena D
+                 ON TRIM(EPS)  = TRIM(D.COD_ENT)  AND TRIM(YEARS) = TRIM(D.YEAR)
+                 LEFT JOIN asegurados_etarios E
+                 ON TRIM(EPS)  = TRIM(E.Cod_entidad)  AND TRIM(YEARS) = TRIM(E.YEAR)
+                 
+     
+                 ")
+    
+    asegurados = eps_homog (Tabla = asegurados , CODIGO__EPS = "eps" )
+    
+    asegurados$Asegurados = as.integer(asegurados$Asegurados)
+    asegurados = subset(asegurados, asegurados$Years != '2009')
+    asegurados = subset(asegurados, asegurados$Years != '2010')
+    asegurados = subset(asegurados, asegurados$Years != '2011')
+    asegurados = subset(asegurados, asegurados$Years != '2021')
+    asegurados = subset(asegurados, asegurados$Years != '2022')
+    asegurados = aggregate_function(aggregate = 'sum',
+                                    cols_to_agg = colnames(asegurados)[3:19]  ,
+                                    group_by = colnames(asegurados[,c(2, 20)]) ,
+                                    Tabla = asegurados)
+    
+    #RES006
+    # CCF010
+    #EPS044 
+    asegurados = sqldf::sqldf("
+                 SELECT fec_inact, fec_liqu, A.* 
+                 FROM asegurados A 
+                 LEFT JOIN fecha_inicio F
+                 ON TRIM(A.homo_code_eps)  = TRIM(F.homo_code_eps)
+                 LEFT JOIN fech_liqui G
+                 ON TRIM(A.homo_code_eps)  = TRIM(G.homo_code_eps)   
+                 ")
+    
+    ## Dropping full nulls
+        
+    # EPS ppara omitir RES011 , FMS001 , POL001  ,ESS115 
+    asegurados_ = data.frame()
+    eps_ = unique(asegurados$homo_code_eps)
+    
+    ####### A este punto tenemos 96 EPS.
+    length(eps_)
+    
+    for (i in eps_) {
+      temp = subset(asegurados, asegurados$homo_code_eps == i)
+      temp = (na_by_cols(temp))
+      temp$homo_code_eps = i
+      asegurados_ = rbind(asegurados_, temp)
+    }
+    eps_nulls =  unique(subset(asegurados_, asegurados_$Asegurados <= 0.8))$homo_code_eps
+    asegurados = subset(asegurados,  asegurados$homo_code_eps   %in%  eps_nulls  )
 
-library(readxl)
-fecha_inicio <- read_excel(paste0(path_output, "fecha_inicio_actividades_administradoras.xlsx" ) ,
-                                                       sheet = "eapb", col_types = c("text", 
-                                                                              "text", "numeric", "text", "numeric", 
-                                                                                     "numeric", "date", "text"))
 
-fecha_inicio = eps_homog (Tabla =  fecha_inicio[, c(7,8)] , CODIGO__EPS = "codigo_supersalud" ) 
-fecha_inicio$fec_inact = as.character(fecha_inicio$fec_inact)
-fecha_inicio$fec_inact = as.numeric(substring(as.character(fecha_inicio$fec_inact), 1, 4))
-fecha_inicio = aggregate_function(aggregate = 'min',
-                                cols_to_agg = colnames(fecha_inicio)[1]  ,
-                                group_by = colnames(fecha_inicio)[3] ,
-                                Tabla = fecha_inicio)
+    
+    eps_ = unique(asegurados$homo_code_eps)
+    length(eps_)
+    ####### A este punto tenemos 91 EPS, 5 eps se pierden pues tienen mas del 80% de nulos.    
+    for (i in colnames(asegurados)[5:20] ) {
+      asegurados[[i]] = ifelse(is.na(asegurados[[5]] )  ==F & is.na(asegurados[[i]]) ==T,  0 , asegurados[[i]] )
+    }
+    for (i in colnames(asegurados)[7:21]) {
+      asegurados[[i]] = as.numeric( as.numeric(asegurados[[i]] ) / as.numeric( asegurados[[6]]) )
+    }
+}    
 
-library(tidyr)
-
-asegurados = asegurados %>% tidyr::pivot_longer(!eps, names_to = "Years", values_to = "Asegurados")         
-asegurados_Tipo_regimen$year = as.character(asegurados_Tipo_regimen$year)
-asegurados_genero$year = as.character(asegurados_genero$year)
-asegurados_com_indigena$year = as.character(asegurados_com_indigena$year)
-asegurados_etarios$year= as.character(asegurados_etarios$year)
-colnames(asegurados_etarios)
- 
-asegurados = sqldf::sqldf("
-             SELECT  A.*,B.TOTAL,  CONTRIBUTIVO , EXCEPCION , SUBSIDIADO   ,
-             FEMENINO, MASCULINO , COMUNIDADES_INDIGENAS,
-             De_0_a_9 , De_10_a_19 , De_20_a_29 , De_30_a_39, 
-             De_40_a_49 , De_50_a_59 , De_60_a_69 ,
-             De_70_a_79 , Mayor_De_80
-             FROM asegurados A
-             LEFT JOIN asegurados_Tipo_regimen B 
-             ON TRIM(EPS)  = TRIM(B.COD_EPS)  AND TRIM(YEARS) = TRIM(B.YEAR)
-             LEFT JOIN asegurados_genero C
-             ON TRIM(EPS)  = TRIM(C.COD_ENTI)  AND TRIM(YEARS) = TRIM(C.YEAR)
-             LEFT JOIN asegurados_com_indigena D
-             ON TRIM(EPS)  = TRIM(D.COD_ENT)  AND TRIM(YEARS) = TRIM(D.YEAR)
-             LEFT JOIN asegurados_etarios E
-             ON TRIM(EPS)  = TRIM(E.Cod_entidad)  AND TRIM(YEARS) = TRIM(E.YEAR)
- 
-             ")
-
-asegurados = eps_homog (Tabla = asegurados , CODIGO__EPS = "eps" )
-
-asegurados$Asegurados = as.integer(asegurados$Asegurados)
-asegurados = subset(asegurados, asegurados$Years != '2009')
-asegurados = subset(asegurados, asegurados$Years != '2010')
-asegurados = subset(asegurados, asegurados$Years != '2011')
-asegurados = subset(asegurados, asegurados$Years != '2021')
-asegurados = subset(asegurados, asegurados$Years != '2022')
-asegurados = aggregate_function(aggregate = 'sum',
-                                cols_to_agg = colnames(asegurados)[3:19]  ,
-                                group_by = colnames(asegurados[,c(2, 20)]) ,
-                                Tabla = asegurados)
-
-#RES006
-# CCF010
-#EPS044 
-asegurados = sqldf::sqldf("
-             SELECT fec_inact, A.* 
-             FROM asegurados A 
-             LEFT JOIN fecha_inicio F
-             ON TRIM(A.homo_code_eps)  = TRIM(F.homo_code_eps)   
-             ")
-glimpse(asegurados)
-for (i in colnames(asegurados)[5:20]) {
-  asegurados[[i]] = ifelse(is.na(asegurados[[4]] )  ==T & asegurados[[2]] <= asegurados[[1]] , 0,   asegurados[[i]]    )
-}
-for (i in colnames(asegurados)[5:20] ) {
-  asegurados[[i]] = ifelse(is.na(asegurados[[4]] )  ==F & is.na(asegurados[[i]]) ==T,  0 , asegurados[[i]] )
-}
-
-for (i in colnames(asegurados)[6:20]) {
-  asegurados[[i]] =  asegurados[[i]]  / asegurados[[5]] 
-}
-# segun la informacion suministrada por SISPRO, los missing value entre 2012 a 2021 son 0
 asegurados_ = data.frame()
 eps_ = unique(asegurados$homo_code_eps)
+
+####### A este punto tenemos 96 EPS.
+length(eps_)
 
 for (i in eps_) {
   temp = subset(asegurados, asegurados$homo_code_eps == i)
@@ -124,12 +202,19 @@ for (i in eps_) {
   temp$homo_code_eps = i
   asegurados_ = rbind(asegurados_, temp)
 }
+
+
+# segun la informacion suministrada por SISPRO, los missing value entre 2012 a 2021 son 0
+
 ### EPS con suficiente informacion de asegurados.
-unique(subset(asegurados_, asegurados_$Asegurados <= 0.5))$homo_code_eps
+ 
+for (i in colnames(asegurados)[5:20]) {
+  asegurados[[i]] = ifelse(is.na(asegurados[[5]] )  ==T & asegurados[[3]] <= asegurados[[2]] , 0,   asegurados[[i]]    )
 }
 
-
-
+asegurados = subset(asegurados,  asegurados$homo_code_eps   %in%   unique(subset(asegurados_, asegurados_$Asegurados <= 0.5))$homo_code_eps )
+#CCF029
+asegurados$homo_code_eps   %in%   unique(subset(asegurados_, asegurados_$Asegurados <= 0.5))$homo_code_eps
 ######################################################################
 
 
